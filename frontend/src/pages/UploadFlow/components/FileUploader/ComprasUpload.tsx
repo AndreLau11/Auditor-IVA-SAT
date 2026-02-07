bff2bb7c2e96b9be2d6617ed410c7c6c28aa7b76
Hector Lau <hectorlau.1993@gmail.com>
2026-02-07 16:24:44 -0600

    all files extension

--- frontend/src/pages/UploadFlow/components/FileUploader/ComprasUpload.tsx
+++ frontend/src/pages/UploadFlow/components/FileUploader/ComprasUpload.tsx
@@ -38,9 +38,13 @@ const ComprasUpload: React.FC<ComprasUploadProps> = (props) => {
         title="Libro de Compras Mensual"
         description="Registro de todas las compras realizadas en el período fiscal"
         acceptedFiles={{
-          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": [".xlsx"],
-          "application/vnd.ms-excel": [".xls"],
-          "text/csv": [".csv"]
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": [".xlsx"], // Excel moderno
+          "application/vnd.ms-excel": [".xls"], // Excel antiguo
+          "application/vnd.ms-excel.sheet.macroEnabled.12": [".xlsm"], // Excel con macros
+          "application/vnd.ms-excel.template.macroEnabled.12": [".xltm"], // Plantillas con macros
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.template": [".xltx"], // Plantillas modernas
+          "application/vnd.ms-excel.addin.macroEnabled.12": [".xlam"], // Complementos de Excel
+          "text/csv": [".csv"] // Archivos CSV
         }}
         requirements={requirements}
         onFileUpload={props.onFileUpload}
